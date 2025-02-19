19be067b41f08394eb2084b05b2d15d51d615791
dr1602 <david.arangelg@gmail.com>
2025-02-19 10:01:46 -0600

    feat: 35 agrego teast unitarios de tdd para popover de termns and conditions

--- src/pages/tests/SummaryForm.test.jsx
+++ src/pages/tests/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SummaryForm } from "../summary/SummaryForm";
 import { expect, test } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -12,12 +13,28 @@ test("Initial conditions", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox disables button on first click and enables on second click", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
-  fireEvent.click(checkbox);
+
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
+
+  await user.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+
+  // popover starts out hidden
+
+  // popover appears on mouseover of checkbox label
+
+  // popover disappers when we mouse out
 });
