d333d34bd6fb0228796ef3c0c43a3874dfa737ab
dr1602 <david.arangelg@gmail.com>
2025-02-19 09:49:21 -0600

    feat: 35 agrego el componente de checkbox para termsn and conditions

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
+const CheckboxLabel = (
+  <span>
+    I agree to <storng style={{ color: "blue" }}> Terms and Conditions </storng>
+  </span>
+);
 
 const SummaryForm = () => {
+  const [tcChecked, setTcChecked] = useState(false);
   return (
     <>
-      <div>
-        <input type="text" />
-        <span>
-          <p> I agree to </p>
-          <strong> Terms and Conditions</strong>
-        </span>
-      </div>
+      <Form>
+        <Form.Group controlId="terms-and-conditions">
+          <Form.Check
+            type="checkbox"
+            checked={tcChecked}
+            onChange={(e) => setTcChecked(e.target.checked)}
+            label={CheckboxLabel}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={!tcChecked}>
+          Confirm order
+        </Button>
+      </Form>
     </>
   );
 };
